641d3b7d53f4554c9a018641288fdc6f3268bf7f
micaels94 <96264952+micaels94@users.noreply.github.com>
2023-11-21 23:10:12 +0000

    Update logic.js
    limiting the numCount to 100

--- logic.js
+++ logic.js
@@ -1,89 +1,83 @@
 document.addEventListener('DOMContentLoaded', function () {
     let currentPosition;
     let gameStarted = false;
-    let numCount = 1; // Initialize numCount to 1
+    let numCount = 1;
+    let lastClickedCell; // Variable to store the last clicked cell
 
     document.querySelectorAll('.grid-cell').forEach(cell => {
         cell.addEventListener('click', function () {
-            if (!gameStarted) {
-                startGame(cell);
-                gameStarted = true;
-            } else {
-                if (!cellIsEmpty(cell) && !isSameCellClicked(cell) && !isCountExceeded()) {
-                    updateGrid(cell, numCount);
-                    highlightValidMoves(currentPosition);
-                    currentPosition = numCount;
-                    numCount += 1;
-                    console.log("It should be doing something this part.")
-                }
-            }
+            handleCellClick(cell);
         });
     });
+
+    function handleCellClick(cell) {
+        console.log("Entering handleCellClick");
+        if (!gameStarted) {
+            startGame(cell);
+            console.log("Game Started");
+        } else {
+            handleCellClickLogic(cell);
+        }
+        console.log("Exiting handleCellClick");
+    }
+
+    function handleCellClickLogic(cell) {
+        if (isSameCellClicked(cell)) {
+            console.log("same cell clicked");
+            // The user clicked in the same cell consecutively
+            // Clear the innerText and set background color to white
+            clearCell(cell);
+        } else if (!cellIsEmpty(cell) && !isCountExceeded()) {
+            console.log("This is a cell argument: " + cell)
+            updateGrid(cell, numCount);
+            currentPosition = numCount;
+            numCount += 1;
+            console.log("not if statement called");
+        }
+    }
 
     function cellIsEmpty(cell) {
         return cell.innerText === '';
     }
 
-    function isSameCellClicked(cell) {
-        return currentPosition === parseInt(cell.innerText, 10);
+    function isSameCellClicked(clickedCell) {
+        let cellClicked = parseInt(clickedCell.innerText);
+        console.log(cellClicked);
+    
+        // Compare the clicked cell with the last clicked cell by their content
+        if (lastClickedCell == cellClicked) {
+            numCount -= 1; // Decrement numCount by 1
+            return true;
+        }
+        return false;
     }
-
-    function isCountExceeded() {
-        return numCount > 100;
+    
+    function isCountExceeded() { // esta função está a funcionar
+        return numCount >= 101;
     }
 
     function startGame(startingCell) {
         currentPosition = parseInt(startingCell.textContent, 10);
         startingCell.innerText = numCount;
         startingCell.classList.add('start-cell');
-        highlightValidMoves(currentPosition);
-        numCount += 1;
+        lastClickedCell = startingCell; // Store the cell reference
+        gameStarted = true; // Set gameStarted to true
     }
 
     function updateGrid(clickedCell, numCount) {
-        // Check if the cell is not empty
-        if (!cellIsEmpty(clickedCell)) {
-            // Reset the cell to its original state
-            clickedCell.innerText = '';
-            clickedCell.style.backgroundColor = 'white';
-        } else {
-            // Place the next number in the clicked cell
-            clickedCell.innerText = numCount.toString();
-            clickedCell.style.backgroundColor = "#e9c46a";
-        }
-        console.log('After updateGrid logic');
-        console.log("This update grid: " + numCount);
+        // Place the next number in the clicked cell
+        console.log("this is the number being incremented");
+        console.log(clickedCell)
+        clickedCell.innerText = numCount.toString();
+        clickedCell.style.backgroundColor = "#e9c46a";
+        lastClickedCell = clickedCell; // Update the last clicked cell
     }
-
-    function highlightValidMoves(currentPosition) {
-        const cells = document.querySelectorAll('.grid-cell');
-        cells.forEach(cell => {
-            if (isValidMove(cell, currentPosition)) {
-                console.log("Is this even being called?")
-                cell.classList.add('valid-move');
-            } else {
-                cell.classList.remove('valid-move');
-            }
-        });
-    }
-
-    function isValidMove(clickedCell, currentPosition) {
-        if (!currentPosition) {
-            return false; // The first move is not a valid move
-        }
-
-        const clickedNumber = parseInt(clickedCell.textContent, 10);
-        const rowDiff = Math.abs(getRow(currentPosition) - getRow(clickedNumber));
-        const colDiff = Math.abs(getCol(currentPosition) - getCol(clickedNumber));
-
-        return (rowDiff === 2 && colDiff === 0) || (colDiff === 2 && rowDiff === 0) || (rowDiff === 1 && colDiff === 1);
-    }
-
-    function getRow(number) {
-        return Math.floor((number - 1) / 10);
-    }
-
-    function getCol(number) {
-        return (number - 1) % 10;
+console.log("This is the var lastClickedCell " + lastClickedCell)
+    function clearCell(cell) {
+        // Reset the cell to its original state
+        console.log("This is the clearCell");
+        cell.innerText = '';
+        cell.style.backgroundColor = 'white';
+        lastClickedCell = null; // Reset the last clicked cell
     }
 });
